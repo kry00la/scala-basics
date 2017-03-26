7586cfc3a9c4f7fcf930bd067e4e3d835c16ea7a
Leonardo Dapdap <l.dapdap@oberthur.com>
2017-03-26 16:34:04 +0200

    added Abstract tester

---
+++ src/main/abstraction/RobotTesterDriver.scala
@@ -0,0 +1,59 @@
+package basic.abstraction
+
+/**
+  * Created by Led on 26/03/2017.
+  */
+
+abstract class Robot {
+  def useSuperWeapon(weapon: String) : Unit
+  def chargeSuperWeapon(weapon: String) : Unit
+}
+
+class SuperHero extends Robot {
+  override def chargeSuperWeapon(weapon: String ="Sword" ): Unit = {
+    println(s" Charging Weapon: $weapon...   ")
+  }
+
+  override def useSuperWeapon(weapon: String = "Sword"): Unit = {
+    println(s" Aiming Super Weapon: $weapon...")
+    println(s" Fire Super Weapon: $weapon...  ")
+  }
+}
+
+class VoltesV extends SuperHero {
+  override def chargeSuperWeapon(weapon: String): Unit = {
+    println(s">> Opening chest compartment for $weapon...")
+    super.chargeSuperWeapon(weapon)
+  }
+}
+
+class MazingerZ extends SuperHero {
+  override def useSuperWeapon(weapon: String): Unit = {
+    println(s">> lift the arm to ready $weapon...")
+    super.useSuperWeapon(weapon)
+  }
+
+  override def chargeSuperWeapon(weapon: String): Unit = {
+    println(s">> initiate full power to activate $weapon...")
+    super.chargeSuperWeapon(weapon)
+  }
+}
+
+object RobotTesterDriver {
+  def main(args: Array[String]) {
+    println("**** Generic Hero ****")
+     var hero = new SuperHero
+     hero.chargeSuperWeapon()
+     hero.useSuperWeapon()
+
+    println("**** VoltesV ****")
+    hero = new VoltesV
+    hero.chargeSuperWeapon("Ultra magnetic top")
+    hero.useSuperWeapon("Ultra magnetic top")
+
+    println("*** MazingerZ ***")
+    val mz = new MazingerZ
+    mz.chargeSuperWeapon("Rocket Punch")
+    mz.useSuperWeapon("Rocket Punch")
+  }
+}
